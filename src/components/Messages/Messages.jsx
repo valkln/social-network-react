ba77202b2a18186eab618ac5f6791dbb79d2b9b6
valkln <kalinin.valentyn96@gmail.com>
2021-09-10 13:55:13 +0300

    messages refactoring

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,8 +1,32 @@
-import s from './Messages.module.css'
-export default function Messages() {
+import s from './Messages.module.css';
+import { NavLink } from 'react-router-dom';
+export default function Messages(props) {
 	return (
 		<div className={s.Messages}>
-			Messages
+			<div className={s.dialogues}>
+				<Dialogue name='Vova' id='1' />
+				<Dialogue name='Yulia' id='2' />
+				<Dialogue name='Vitya' id='3' />
+				<Dialogue name='Arsen' id='4' />
+			</div>
+			<div className={s.messaging}>
+				<Message message="I wasn't re-elected" />
+				<Message message='How are you?' />
+				<Message message='Hi' />
+				<Message message='I thought we were in love' />
+			</div>
 		</div>
 	);
+}
+
+function Dialogue(props) {
+	let path = '/messages/' + props.id;
+	return (
+		<div className={s.dialogue}><NavLink to={path}>{props.name}</NavLink></div>
+	)
+}
+
+function Message(props) {
+
+	return <div className={s.message}>{props.message}</div>
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css'
 export default function Navbar() {
 	return (
 		<nav className={s.nav}>
 			<ul>
-				<a href='/profile' className={s.link}><li className={s.item}>Profile</li></a>
-				<a href='/messages' className={s.link}><li className={s.item}>Messages</li></a>
-				<a href='/news' className={s.link}><li className={s.item}>News</li></a>
-				<a href='/music' className={s.link}><li className={s.item}>Music</li></a>
-				<a href='/settings' className={s.link}><li className={s.item}>Settings</li></a>
+				<NavLink to='/profile' activeClassName={s.active} className={s.link}><li className={s.item}>Profile</li></NavLink>
+				<NavLink to='/messages' activeClassName={s.active} className={s.link}><li className={s.item}>Messages</li></NavLink>
+				<NavLink to='/news' activeClassName={s.active} className={s.link}><li className={s.item}>News</li></NavLink>
+				<NavLink to='/music' activeClassName={s.active} className={s.link}><li className={s.item}>Music</li></NavLink>
+				<NavLink to='/settings' activeClassName={s.active} className={s.link}><li className={s.item}>Settings</li></NavLink>
 			</ul>
 		</nav>
 	);
