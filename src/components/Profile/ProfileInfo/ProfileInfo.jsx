2337e00fcd357159578ebb96b3fc4be20f42e519
valkln <kalinin.valentyn96@gmail.com>
2021-10-13 17:12:26 +0300

    created status with edit mode

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 import s from './ProfileInfo.module.css'
 import defUserPic from '../../../img/ava.png'
+import ProfileStatus from './ProfileStatus';
 const ProfileInfo = (props) => {
 	return (
 		<div className={s.profile}>
@@ -7,6 +8,7 @@ const ProfileInfo = (props) => {
 				<div className={s.userpic}><img src={props.profile.photos.large !== null ? props.profile.photos.large : defUserPic} alt='userpic'></img></div>
 				<div className={s.userinfo}>
 					<div className={s.fullName}>{props.profile.fullName}</div>
+					<ProfileStatus status={'test'} />
 					<div>{props.profile.aboutMe}</div>
 					<div className={s.contacts}></div>
 				</div>
---
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+class ProfileStatus extends React.Component {
+	state = {
+		editMode: false
+	}
+	activateEditMode() {
+		this.setState({
+			editMode: true
+		})
+	}
+	deactivateEditMode() {
+		this.setState({
+			editMode: false
+		})
+	}
+	render() {
+		return (
+			<div>
+				{this.state.editMode ?
+					<div>
+						<input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} type="text" value={this.props.status} />
+					</div>
+					:
+					<div>
+						<span onClick={this.activateEditMode.bind(this)} >{this.props.status}</span>
+					</div>
+				}
+			</div>
+		);
+	}
+}
+export default ProfileStatus;
